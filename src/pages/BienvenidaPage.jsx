bd944c10d795a2e3e0fda9f4717c35b27a6a8c61
toledanogus <toledanogu@gmail.com>
2024-02-11 16:45:48 -0600

    Beta 1.1

--- src/pages/BienvenidaPage.jsx
+++ src/pages/BienvenidaPage.jsx
@@ -1,6 +1,6 @@
-import { useContext } from 'react';
-import { useNavigate } from 'react-router-dom'; // Importa el hook useNavigate
-import { UserContext } from '../context/createContext';
+import { useContext } from "react";
+import { useNavigate } from "react-router-dom"; // Importa el hook useNavigate
+import { UserContext } from "../context/createContext";
 
 export const BienvenidaPage = () => {
   const { nombre, setNombre } = useContext(UserContext);
@@ -11,19 +11,24 @@ export const BienvenidaPage = () => {
   };
 
   const siguiente = () => {
-    navigate('/sumas'); // Navega a la ruta /sumas
+    navigate("/sumas"); // Navega a la ruta /sumas
   };
 
   return (
     <>
       <h1>¡Bienvenido!</h1>
       <fieldset>
-      <p>Comencemos escribiendo tu nombre de usuario:</p>
-      <input type="text" id="nombre" value={nombre} onChange={onInputChange2} />
-      <p>Tienes un minuto para realizar la mayor cantidad de sumas.</p>
-      <button type="button" className="comenzar" onClick={siguiente}>
-        Comenzar
-      </button>
+        <p>Comencemos escribiendo tu nombre de usuario:</p>
+        <input
+          type="text"
+          id="nombre"
+          value={nombre}
+          onChange={onInputChange2}
+        />
+        <p>Tienes un minuto para realizar la mayor cantidad de sumas.</p>
+        <button type="button" className="comenzar" onClick={siguiente}>
+          Comenzar
+        </button>
       </fieldset>
     </>
   );
--- src/pages/ResultadosPage.jsx
+++ src/pages/ResultadosPage.jsx
@@ -1,11 +1,13 @@
 import { useContext, useEffect, useState } from "react";
 import { UserContext } from "../context/createContext";
+import { useNavigate } from "react-router-dom"; 
 
 export const ResultadosPage = () => {
-  const { nombre, puntaje1 } = useContext(UserContext);
+  const { nombre, puntaje1, setNombre, setPuntaje1} = useContext(UserContext);
   const [datos, setDatos] = useState(null);
   const url = "../SumasReact/php/enviar.php";
   const url2 = "../SumasReact/php/recibir.php";
+  const navigate = useNavigate(); 
 
   let datosJson = {
     nom: nombre,
@@ -33,51 +35,59 @@ export const ResultadosPage = () => {
     console.log(data); // Verifica la respuesta en la consola
   };
 
+  const reiniciar =  () => {
+    setDatos(null);
+    setNombre('');
+    setPuntaje1(0);
+    navigate("/Bienvenida");
+
+  }
+
   useEffect(() => {
-    enviarNombre().then(() => recibirData());
+    if (nombre !== '') {
+      enviarNombre().then(() => recibirData());
+    }
     // eslint-disable-next-line react-hooks/exhaustive-deps
   }, []); // Dependencias actualizadas
 
   return (
     <>
       <h1>¡Resultados!</h1>
-      <h4>
-        Hola {nombre}, tu puntaje es de {puntaje1}
-      </h4>
+      <h4>Hola {nombre}, tu puntaje es de {puntaje1}</h4>
       <div className="tablafinal">
-        {/* Usa un operador lógico y un operador de acceso opcional para verificar que datos tenga el elemento con el índice que buscas */}
-        {datos?.[0] && (
-          <p>
-            Primer Lugar: <span className="bold">{datos[0].nombre}</span> con un puntaje de {datos[0].puntaje}
-          </p>
-        )}
-        {datos?.[1] && (
-          <p>
-            Segundo Lugar: {datos[1].nombre} con un puntaje de{" "}
-            {datos[1].puntaje}
-          </p>
-        )}
-        {datos?.[2] && (
-          <p>
-            Tercer Lugar: {datos[2].nombre} con un puntaje de {datos[2].puntaje}
-          </p>
-        )}
-        {datos?.[3] && (
-          <p>
-            Cuarto Lugar: {datos[3].nombre} con un puntaje de {datos[3].puntaje}
-          </p>
-        )}
-        {datos?.[4] && (
-          <p>
-            Quinto Lugar: {datos[4].nombre} con un puntaje de {datos[4].puntaje}
-          </p>
-        )}
-        {datos?.[5] && (
-          <p>
-            Sexto Lugar: {datos[5].nombre} con un puntaje de {datos[5].puntaje}
-          </p>
-        )}
+        <table>
+          <thead>
+            <tr>
+              <th>Lugar</th>
+              <th>Nombre</th>
+              <th className="centered">Puntaje</th> {/* Agregamos una clase para centrar */}
+            </tr>
+          </thead>
+          <tbody>
+            {/* Utiliza un bucle para recorrer los primeros diez datos y mostrar las filas de la tabla */}
+            {datos?.slice(0, 10).map((dato, index) => (
+              <tr key={index}>
+                <td>
+                  {index === 0 && "1º"}
+                  {index === 1 && "2º"}
+                  {index === 2 && "3º"}
+                  {index === 3 && "4º"}
+                  {index === 4 && "5º"}
+                  {index === 5 && "6º"}
+                  {index === 6 && "7º"}
+                  {index === 7 && "8º"}
+                  {index === 8 && "9º"}
+                  {index === 9 && "10º"}
+                </td>
+                <td>{dato.nombre}</td>
+                <td className="centered">{dato.puntaje}</td> {/* Aplicamos la clase para centrar */}
+              </tr>
+            ))}
+          </tbody>
+        </table>
       </div>
+      <button onClick={reiniciar}>Volver a jugar</button>
     </>
   );
+  
 };
